fa3349fa223666aa58a6e0a95b9531c2a883f3e1
leslieyi <leslieyi411@gmail.com>
2021-08-31 12:40:53 -0400

    commit tree 3933e471014f1b814b1768522c0a1a7b6fbd84e2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    # validate :clickbait?
+    
+
+
+    # CLICKBAIT_PATTERNS = [
+    #     /Won't Believe/i, #i means ignore case
+    #     /Secret/i,
+    #     /Top \d/i,
+    #     /Guess/i
+    #   ]
+    
+    #   def clickbait?
+    #     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    #       errors.add(:title, "must be clickbait")
+    #     end
+    #   end
+
+      validate :is_clickbait
+
+      def is_clickbait
+          unless title.present? && title.match?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+              errors.add(:title, "No journalistic integrity allowed")
+          end
+      end
+
 end
